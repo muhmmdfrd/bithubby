58634c921c0a66520d1694d92ee613cdf2f8ce20
muhmmdfrd <mfarid2121@gmail.com>
2023-06-19 09:16:45 +0700

    add identifier for specific user logrocket

--- static/logrocket.js
+++ static/logrocket.js
@@ -1,1 +1,19 @@
 window.LogRocket && window.LogRocket.init("zxwdzu/bithubby");
+
+LogRocket.identify("6b8f2e63-0cb7-49a8-872b-85e05f1b2277", {
+  name: "Syara Azzahra (UC Browser)",
+  email: "syaraazzahraa@gmail.com",
+  favorite: true,
+});
+
+LogRocket.identify("19946ba1-4677-422a-b9e7-6939d1acddc9", {
+  name: "Syara Azzahra (HP Kantor)",
+  email: "syaraazzahraa@gmail.com",
+  favorite: true,
+});
+
+LogRocket.identify("033a7e9a-cf3b-4c38-a73b-8a571532b2be", {
+  name: "Lina Hasna",
+  email: "linahna@gmail.com",
+  favorite: false,
+});
