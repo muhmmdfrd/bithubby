2e24b228eae19500d05bac42c0e846f7d5a26492
Muhammad Farid <mfarid2121@gmail.com>
2024-07-29 08:22:52 +0700

    using image cdn

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -5,9 +5,9 @@ import BlogPostItemContainer from "./Container";
 import BlogPostItemHeader from "./Header";
 import BlogPostItemContent from "./Content";
 import BlogPostItemFooter from "./Footer";
-// @ts-ignore
 import Image from "@theme/IdealImage";
-// apply a bottom margin in list view
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+
 function useContainerClassName() {
   const { isBlogPostPage } = useBlogPost();
   return !isBlogPostPage ? "margin-bottom--xl" : undefined;
@@ -16,10 +16,16 @@ export default function BlogPostItem({ children, className }) {
   const containerClassName = useContainerClassName();
   const { frontMatter, assets } = useBlogPost();
   const image = assets.image ?? frontMatter.image;
+  const {
+    siteConfig: { customFields },
+  } = useDocusaurusContext();
+
+  const imageUrl = customFields.cdnImageUrl.toString() + image;
+
   return (
     <BlogPostItemContainer className={clsx(containerClassName, className)}>
       <BlogPostItemHeader />
-      {image && <Image img={image} />}
+      {image && <Image img={imageUrl} />}
       <BlogPostItemContent>{children}</BlogPostItemContent>
       <BlogPostItemFooter />
     </BlogPostItemContainer>
